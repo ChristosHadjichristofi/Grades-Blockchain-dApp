a992f0d14bed6202c20df740853ccd18731a3a44
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-03-14 21:56:58 +0200

    Show courses depending on the wallet

--- controllers/layout.js
+++ controllers/layout.js
@@ -1,5 +1,11 @@
 const { web3Object } = require('../utils/web3');
 
+// require models
+const sequelize = require('../utils/database');
+var initModels = require("../models/init-models");
+var models = initModels(sequelize);
+// end of require models
+
 exports.getIndex = (req, res, next) => {
     res.render('index.ejs', {
         pageTitle: "Landing Page"
@@ -13,10 +19,62 @@ exports.getForm = (req, res, next) => {
 }
 
 exports.getCourses = (req, res, next) => {
-    let courses = [];
+    let schools = {};
 
-    res.render('courses.ejs', {
-        pageTitle: "Courses Page",
-        courses
-    });
+    models.permissions.findOne({
+        raw: true,
+        include: [{
+            model: models.users,
+            on: {
+                col1: sequelize.where(sequelize.col("id"), "=", sequelize.col("permissions.users_id"))
+            },
+            where: { wallet: process.env.LOCAL_NODE_ADDR }
+        }] 
+    })
+    .then(userInfo => {
+        console.log(userInfo)
+        if (userInfo.isMaster) {
+            return models.courses.findAll({
+                raw: true,
+                include: [{
+                    model: models.users,
+                    on: {
+                        col1: sequelize.where(sequelize.col("id"), "=", sequelize.col("courses.users_id"))
+                    }
+                }]
+            })
+        }
+        else {
+            return models.courses.findAll({
+                raw: true,
+                include: [{
+                    model: models.users,
+                    on: {
+                        col1: sequelize.where(sequelize.col("id"), "=", sequelize.col("courses.users_id"))
+                    },
+                    where: { id: userInfo.users_id }
+                }]
+            })
+        }
+    })
+    .then(coursesArr => {
+        for (const course of coursesArr) {
+            if (!schools.hasOwnProperty(course['user.belongsTo'])) {
+                schools[course['user.belongsTo']] = []
+            }
+            schools[course['user.belongsTo']].push({ code: course.code.toString(), name: course.name })
+            // let c = {
+            //     code: course.code.toString(),
+            //     name: course.name,
+            //     belongsTo: course['user.belongsTo']
+            // };
+            // courses.push(c);
+        }
+        res.render('courses.ejs', {
+            pageTitle: "Courses Page",
+            schools
+        });
+
+    })
+
 }
